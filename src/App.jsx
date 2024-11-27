55f433f00570acfdfdc68b95b25f7330972f0c41
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-11-27 17:57:40 +0100

    Revert "add bonus"
    This reverts commit 411f8a340ace5bb0651f201825c42434025e043e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import tasks from "./data/tasks";
-import classNames from "classnames";
 
 function App() {
   const completedTasks = tasks.filter((task) => task.state == "completed");
   console.log(completedTasks);
   const currentTasks = tasks.filter((task) => task.state !== "completed");
-
-  const assignStateClass = (state) => {
-    return classNames({
-      "status-label": true, // Always apply 'status-label'
-      "completed-task": state === "completed", // Apply 'completed' if state is "completed"
-      "in-progress-task": state === "in_progress", // Apply 'in-progress' if state is "in-progress"
-      "backlog-task": state === "backlog", // Apply 'backlog' if state is "backlog"
-    });
-  };
 
   return (
     <>
@@ -31,12 +21,11 @@ function App() {
           </h2>
           <ul>
             {currentTasks.map((task) => {
-              const taskClass = assignStateClass(task.state);
-
               return (
                 <li key={task.id} className="task-item">
                   <div className="task-title">
-                    {task.title} <span className={taskClass}>{task.state}</span>
+                    {task.title}{" "}
+                    <span className="status-label">{task.state}</span>
                   </div>
                   <div>Priority: {task.priority}</div>
                   <div>Est. time {task.estimatedTime}</div>
@@ -54,12 +43,14 @@ function App() {
           </h2>
           <ul>
             {completedTasks.map((task) => {
-              const taskClass = assignStateClass(task.state);
-
+              {
+                task.title;
+              }
               return (
                 <li key={task.id} className="task-item">
                   <div className="task-title">
-                    {task.title} <span className={taskClass}>{task.state}</span>
+                    {task.title}{" "}
+                    <span className="status-label">{task.state}</span>
                   </div>
                   <div>Priority: {task.priority}</div>
                   <div>Est. time {task.estimatedTime}</div>
