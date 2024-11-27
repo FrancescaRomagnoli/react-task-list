841aee8995ef84dcd8c8e8dbbc4c11b3ec59e039
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-11-27 17:23:32 +0100

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,57 @@ function App() {
 
   return (
     <>
-      <div>
-        <h2>Current Tasks</h2>
-        <ul>
-          {currentTasks.map((task) => {
-            return (
-              <li key={task.id}>
-                <div>
-                  {task.title} <span>{task.state}</span>
-                </div>
-                <div>Priority: {task.priority}</div>
-                <div>Est. time {task.estimatedTime}</div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div>
-        <h2>Completed Tasks</h2>
-        <ul>
-          {completedTasks.map((task) => {
-            {
-              task.title;
-            }
-            return (
-              <li key={task.id}>
-                <div>
-                  {task.title} <span>{task.state}</span>
-                </div>
-                <div>Priority: {task.priority}</div>
-                <div>Est. time {task.estimatedTime}</div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <header className="header">
+        <div className="container">
+          <h1>Task Manager</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <h2 className="tasks-cat-title">
+            Current Tasks ({currentTasks.length})
+          </h2>
+          <ul>
+            {currentTasks.map((task) => {
+              return (
+                <li key={task.id} className="task-item">
+                  <div className="task-title">
+                    {task.title}{" "}
+                    <span className="status-label">{task.state}</span>
+                  </div>
+                  <div>Priority: {task.priority}</div>
+                  <div>Est. time {task.estimatedTime}</div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="container">
+          <hr />
+        </div>
+        <div className="container">
+          <h2 className="tasks-cat-title">
+            Completed Tasks({completedTasks.length})
+          </h2>
+          <ul>
+            {completedTasks.map((task) => {
+              {
+                task.title;
+              }
+              return (
+                <li key={task.id} className="task-item">
+                  <div className="task-title">
+                    {task.title}{" "}
+                    <span className="status-label">{task.state}</span>
+                  </div>
+                  <div>Priority: {task.priority}</div>
+                  <div>Est. time {task.estimatedTime}</div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
     </>
   );
 }
